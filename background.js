2cc89f7c5d5af93d9f33446ac918b19d920a30c3
Nathan Pitman <pitmannathan@gmail.com>
2014-12-15 00:47:16 +1300

    It works!

--- background.js
+++ background.js
@@ -1,23 +1,25 @@
-var extension = (function() {
-    var lastRequest = null;
+(function() {
+    var thresholdInMilliseconds = 500,        // TODO: Make configurable.
+        lastRequest = null;
 
-    return {
-        logRequest: function(url, time) {
-        }
-    };
+    chrome.webNavigation.onBeforeNavigate.addListener(function(details) {
+        // We only care about user instigated navigation which launches new tabs/windows, not iframe / AJAX requests.
+        if (details.parentFrameId !== -1) 
+            return;
+
+        if (shouldCancel(details.url, details.timeStamp))
+            chrome.tabs.remove(details.tabId);
+        else
+            logRequest(details.url, details.timeStamp);
+    }, null, ["blocking"]);
+
+    
+    function shouldCancel(url, timeStamp) {
+       return lastRequest && lastRequest.url === url && thresholdInMilliseconds > (timeStamp - lastRequest.timeStamp);
+    }
+
+
+    function logRequest(url, timeStamp) {
+        lastRequest = { url: url, timeStamp: timeStamp };
+    }
 })();
-
-
-chrome.webNavigation.onBeforeNavigate.addListener(function(details) {
-    console.log('URL = ' + details.url);
-    console.log('timestamp = ' + details.timeStamp);
-    console.log('frameId = ' + details.frameId);
-    console.log('parentFrameId = ' + details.parentFrameId);
-    console.log('tabId = ' + details.tabId);
-
-
-    return {
-        cancel: true
-        //redirectUrl: 'http://www.google.co.nz'
-    };
-}, null, ["blocking"]);
