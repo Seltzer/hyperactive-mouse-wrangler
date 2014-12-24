c6e040f2fffba0b0bb6ed5f5f79b390b3b3dbe5e
Nathan Pitman <pitmannathan@gmail.com>
2014-12-24 22:56:31 +1300

    Change fundamentals. Now we're using tabs.onCreated instead of webNavigation.onBeforeNavigate

--- background.js
+++ background.js
@@ -2,24 +2,47 @@
     var thresholdInMilliseconds = 500,        // TODO: Make configurable.
         lastRequest = null;
 
-    chrome.webNavigation.onBeforeNavigate.addListener(function(details) {
-        // We only care about user instigated navigation which launches new tabs/windows, not iframe / AJAX requests.
-        if (details.parentFrameId !== -1) 
-            return;
-
-        if (shouldCancel(details.url, details.timeStamp))
-            chrome.tabs.remove(details.tabId);
-        else
-            logRequest(details.url, details.timeStamp);
-    }, null, ["blocking"]);
+    /**
+     * We're hooking into the tabs.onCreated event in order to figure out when tabs have been spawned via middle click
+     * (and by other means). Alternatives:
+     *
+     * 1. webNavigation.onBeforeNavigate - Convenient as it provides a URL and timestamp for us. Problems/disadvantages:
+     *        - Fires too late; after the tab has been spawned, after tabs.onCreated.
+     *        - Need to ignore activity which doesn't spawn a new tab (e.g. repeated refreshes in current tab).
+     *        - Need to ignore intra-tab navigation (i.e. navigation not triggered by a user; e.g. iframe/AJAX requests)
+     */
+    chrome.tabs.onCreated.addListener(function(tab) {
+        sanctionTab(tab, generateTimestamp());
+    });
 
     
-    function shouldCancel(url, timeStamp) {
-       return lastRequest && lastRequest.url === url && thresholdInMilliseconds > (timeStamp - lastRequest.timeStamp);
+    function generateTimestamp() {
+        // We're doing this because the above event doesn't give us a timestamp and the alarms API isn't granular enough.
+        return new Date().getTime();
     }
 
 
-    function logRequest(url, timeStamp) {
-        lastRequest = { url: url, timeStamp: timeStamp };
+    /**
+    * @param tab (chrome.tabs.Tab)
+    */
+    function sanctionTab(tab, timestamp) {
+        // Ignore new blank tabs... they're all good.
+        if (!tab.url)
+            return;
+
+        if (shouldCancel(tab.url, timestamp))
+            chrome.tabs.remove(tab.id);
+        else
+            logRequest(tab.url, timestamp);
+    }
+
+    
+    function shouldCancel(url, timestamp) {
+        return lastRequest && lastRequest.url === url && thresholdInMilliseconds > (timestamp - lastRequest.timestamp);
+    }
+
+
+    function logRequest(url, timestamp) {
+        lastRequest = { url: url, timestamp: timestamp };
     }
 })();
